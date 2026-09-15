74afdf3462ab8416bafb61944039c063f30d32ce
saravanansn2622007-boop <saravanansn2622007@gmail.com>
2026-09-15 10:19:57 +0530

    feat(buyer): update authentic produce image for Beed High-Oil Sunflower Seeds

--- buyer-module/js/data.js
+++ buyer-module/js/data.js
@@ -441,7 +441,7 @@ const buyerData = {
       farmerLocation: "Beed APMC Yard ~ 7.2 km away",
       farmerRating: "4.83 ⭐",
       farmerPhone: "+91 98221-88900",
-      image: "assets/images/sunflower.jpg?v=1",
+      image: "assets/images/sunflower.jpg?v=2",
       grade: "Grade A Oil >40%",
       gradeKey: "grade-a",
       gradeBadgeClass: "badge-grade-a",
